b6ebc3f5479abe2fa0bbb6cf7b030ba58a3822ab
hsptarun <tarunpal1025@gmail.com>
2025-04-29 19:02:01 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,12 @@
+A = input('Enter the first number: ' )
+B = input('Enter the second number: ' )
+A = int(A)
+B = int(B)
+a = A + B
+s = A - B
+m = A * B
+d = A/B
+print ("Addition: ",a)
+print ("Subtraction: ",s)
+print ("Multiplication: ",m)
+print ("Division: ",d)
